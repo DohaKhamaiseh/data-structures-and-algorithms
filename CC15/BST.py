67f6d858cda531b62c0a4874fb400c550d2ca31f
DohaKhamaiseh <dohakhamaiseh1999@gmail.com>
2023-05-24 20:15:30 +0000

    need edit

--- CC15/BST.py
+++ CC15/BST.py
@@ -67,27 +67,36 @@ class BST(BinaryTree) :
 tree_test = BinaryTree()
 
 
-Node1 = Node(10)
+Node1 = Node(2)
 tree_test.root = Node1
 
-Node2 = Node(5)
+Node2 = Node(7)
 tree_test.root.left = Node2
 
-Node3 = Node(15)
+Node3 = Node(5)
 tree_test.root.right = Node3
 
-# Node4 = Node(4)
-# tree_test.root.left.left = Node4
+Node4 = Node(2)
+tree_test.root.left.left = Node4
 
-# Node5 = Node(5)
-# tree_test.root.left.right = Node5
+Node5 = Node(6)
+tree_test.root.left.right = Node5
 
-# Node6 = Node(6)
-# tree_test.root.right.left = Node6
+Node5_1 = Node(5)
+tree_test.root.left.right.left = Node5_1
 
-print(tree_test.pre_order(tree_test.root))
-print(tree_test.in_order(tree_test.root))
-print(tree_test.post_order(tree_test.root))
+Node5_2 = Node(11)
+tree_test.root.left.right.right = Node5_2
+
+Node6 = Node(9)
+tree_test.root.right.right = Node6
+
+Node6_1 = Node(4)
+tree_test.root.right.right.left = Node6_1
+
+# print(tree_test.pre_order(tree_test.root))
+# print(tree_test.in_order(tree_test.root))
+# print(tree_test.post_order(tree_test.root))
 
 
 # BST_test = BST()
@@ -104,7 +113,8 @@ print(tree_test.post_order(tree_test.root))
 # print(BST_test.post_order(BST_test.root))
 
 # print(BST_test.contains(5))
-print(tree_test.getMax())
+# print(tree_test.getMax())
+print(tree_test.tree_breadth_first(tree_test.root))
 
 
 
